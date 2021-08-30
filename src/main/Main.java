79e3328ae5cddf8172b4232c2762ef58318156a8
kwanghun <s5095940@naver.com>
2021-08-30 21:52:02 +0900

    첫번째 커밋

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,13 @@ package main;
 
 import java.util.Scanner;
 
+import Skh.Skh_mb;
+
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		Skh_mb skh = new Skh_mb();
 		int num;
 		while(true) {
 			System.out.println("1.이주영님 2.성기룡님 3.설지희님 4.서광훈님 5.송영관님");
@@ -14,7 +17,8 @@ public class Main {
 			case 1:break;
 			case 2:break;
 			case 3:break;
-			case 4:break;
+			case 4: skh.print();
+				break;
 			case 5: break;
 			}
 		}
