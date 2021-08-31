c502a0ac434c1810cea652594b1b49466fcd5ca1
seoljihee <wlgml7997@naver.com>
2021-08-31 19:26:31 +0900

    삭제기능 추가

--- src/jihee/Jihee_Student.java
+++ src/jihee/Jihee_Student.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 
 public class Jihee_Student {
 
+	
 	public static void main(String[] args) {
 
 		String studentNum, name, add, phoneNum;
+		String a;
 		ArrayList<StudentDTO> list = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("--- 설지희 학생 관리 프로그램 ---");
 		while(true) {
-			System.out.println("1.등록 2.목록 3.종료");
+			System.out.println("1.등록하기 2.목록보기 3.수정");
+			System.out.println("4.삭제하기 5.검색하기 6.종료");
 			System.out.print(">>>>");
 			int num = sc.nextInt();
 			switch(num) {
@@ -36,32 +39,52 @@ public class Jihee_Student {
 				
 				break;
 			case 2:
-				System.out.println("--- 학생 목록 보기 ---");
+				if(list.size() == 0) {
+					System.out.println("등록 된 회원정보가 없습니다.\n등록을 먼저 진행하세요.");
+				}else {
+				System.out.println("--- 전체학생 목록 보기 ---");
+					for(int i=0; i<list.size(); i++) {
+						System.out.println("학번: " + list.get(i).getStudentNum());
+						System.out.println("이름: " + list.get(i).getName());
+						System.out.println("주소: " + list.get(i).getAdd());
+						System.out.println("전화번호: " + list.get(i).getPhoneNum());
+						System.out.println("======================");
+					}
+				}
+				break;
+				
+				
+			case 3: 
+				System.out.println("");
+				break;
+				
+			case 4: 
+				
+				if(list.size() == 0) {
+					System.out.println("등록 된 회원정보가 없습니다.\n등록을 먼저 진행하세요.");
+				}else {
+				System.out.println("삭제할 학생의 이름을 입력하세요.");
+				System.out.print(">>>>");
+				a = sc.next();
 				for(int i=0; i<list.size(); i++) {
+				if(a.equals(list.get(i).getName())) {
+					System.out.println("*** 삭제가 완료된 항목 ***");
 					System.out.println("학번: " + list.get(i).getStudentNum());
 					System.out.println("이름: " + list.get(i).getName());
 					System.out.println("주소: " + list.get(i).getAdd());
 					System.out.println("전화번호: " + list.get(i).getPhoneNum());
-					System.out.println("======================");
+					System.out.println("=======================");					
+					System.out.println("위 내용이 삭제 완료되었습니다.");
+					list.remove(list.get(i));
+					break;
+				}else {
+					System.out.println("존재하지 않는 회원입니다.");
 				}
-				System.out.println("1.삭제  2.수정  3.뒤로가기");
-				System.out.print(">>>>");
-				num = sc.nextInt();
-				while(true) {
-					switch(num) {
-					case 1:
-						System.out.println("삭제하려는 회원의 이름을 입력하시오.");
-						System.out.print(">>>>");
-						String reName = sc.next();
-						break;
-					case 2:break;
-					case 3:
-						return;
-					}
 				}
-				
-				
-			case 3:
+				}
+				break;
+			case 5: break;
+			case 6: 
 				System.out.println("프로그램을 종료합니다.");
 				return;
 			}
